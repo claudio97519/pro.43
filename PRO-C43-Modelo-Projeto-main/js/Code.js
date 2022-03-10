37b575c04dd067ef5157e060f270fb12c51a9f5d
claudio97519 <93736562+claudio97519@users.noreply.github.com>
2022-03-10 16:53:18 -0300

    Add files via upload

---
+++ PRO-C43-Modelo-Projeto-main/js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "Pedro";
+const accessCode2 = "14";
+const accessCode3 = "cachorro";
+
+function clues() {
+    fill("white");
+    textSize(15);
+    text("Questionario", 100,50);
+    fill("lightblue")
+    text("primeira pergunta:Qual é seu nome?", 100,70);
+// adicione código para exibir o restante dos nomes e as dicas.
+
+    fill("white");
+    textSize(15);
+    fill("lightblue");
+    text("segunda pergunta:Qual é a sua idade?", 700,170);
+
+    fill("white");
+    textSize(15);
+    fill("lightblue");
+    text("terceira pergunta:Qual seu animal de estimação?", 100,270);
+    }
---
+++ PRO-C43-Modelo-Projeto-main/js/js2/Code.js
@@ -0,0 +1,20 @@
+const accessCode1 = "Pedro";
+const accessCode2 = "14";
+const accessCode3 = "cachorro";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Realiza uma tarefa em particular!", 700, 170);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Guarda toda a informação!", 100, 270);
+}
